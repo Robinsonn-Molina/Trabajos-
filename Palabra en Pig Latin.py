1e4d4ca1efe20607c816a73d505aaae93bacd632
Robinsonn-Molina <robinson2250159@correo.uis.edu.co>
2025-05-16 20:57:44 -0500

    Update Palabra en Pig Latin.py

--- Palabra en Pig Latin.py
+++ Palabra en Pig Latin.py
@@ -1,6 +1,6 @@
 palabra = input("Ingrese una palabra: ")
 
-if palabra[0] in "aeiouAEIOU": #revisar si use adecuadamente el 0 en llaves y el in 
+if palabra[0] in "aeiouAEIOU": #revisar si usa adecuadamente el 0 en llaves y el in 
     resultado = palabra + "way"
 else:
     resultado = palabra[1:] + palabra[0] + "ay"
